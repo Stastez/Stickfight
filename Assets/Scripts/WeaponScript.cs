23be0d30a4af200e63fa125b844a47e0004b1c87
Stastez <sandro.steeger@student.hpi.de>
2022-06-30 15:50:35 +0200

    Added kill and block sound

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -1,25 +1,39 @@
 ﻿using System;
+using System.Threading;
 using SpeechIO;
 using UnityEngine;
 
 public class WeaponScript : MonoBehaviour
 {
-    private BoxCollider _collider;
-    private GameObject _enemy;
+    private BoxCollider _ownCollider;
+    private GameObject _ownGameObject, _enemy, _enemyWeapon;
     private SpeechOut _speech;
+    private AudioSource _audioSource;
+    public AudioClip enemyKilled, enemyBlocked;
 
     private void Start()
     {
-        _collider = GameObject.Find("PlayerWeapon").GetComponent<BoxCollider>();
+        _ownGameObject = GameObject.Find("PlayerWeapon");
+        _ownCollider = _ownGameObject.GetComponent<BoxCollider>();
         _enemy = GameObject.Find("Enemy");
         _speech = new SpeechOut();
+        _enemyWeapon = GameObject.Find("EnemyWeapon");
+        _audioSource = _ownGameObject.GetComponent<AudioSource>();
     }
 
     private async void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.gameObject.Equals(_enemy))
+        GameObject collidedGameObject = collision.collider.gameObject;
+        
+        if (_enemyWeapon != null && collidedGameObject.Equals(_enemyWeapon))
         {
+            _audioSource.PlayOneShot(enemyBlocked);
+        }
+        else if (collidedGameObject.Equals(_enemy))
+        {
+            _audioSource.PlayOneShot(enemyKilled);
             GameObject.Destroy(_enemy);
+            Thread.Sleep((int) (enemyKilled.length * 1000));
             await _speech.Speak("You have slain your enemy!");
         }
     }
