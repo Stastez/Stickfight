d6ede8fa4a0d079ebcb498b1a7be91e1f8c8737f
Stastez <sandro.steeger@student.hpi.de>
2022-06-30 16:45:24 +0200

    Made enemy have delayed response

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,27 +4,30 @@ using DualPantoFramework;
 public class EnemyScript : MonoBehaviour
 {
     private PantoHandle _itHandle, _meHandle;
-    private GameObject _enemy, _player;
-    private GameObject _weapon;
+    private GameObject _enemy, _player, _weapon;
+    private Vector3 _oldPosition;
+    private float _oldRotation;
 
     // Start is called before the first frame update
     async void Start()
+    {
+        FindGameObjects();
+
+        _itHandle.Free();
+        await _itHandle.SwitchTo(_enemy);
+        
+        InvokeRepeating(nameof(PositionWeapon), 0, 2);
+    }
+
+    private void FindGameObjects()
     {
         _meHandle = GameObject.Find("Panto").GetComponent<UpperHandle>();
         _itHandle = GameObject.Find("Panto").GetComponent<LowerHandle>();
         _enemy = GameObject.Find("Enemy");
         _player = GameObject.Find("Player");
         _weapon = GameObject.Find("EnemyWeapon");
-
-        _itHandle.Free();
-        await _itHandle.SwitchTo(_enemy);
     }
 
-    private void FixedUpdate()
-    {
-        PositionWeapon();
-    }
-    
     private void PositionWeapon()
     {
         var playerIsLeftOfEnemy = _player.transform.position.x <= _enemy.transform.position.x;
@@ -33,7 +36,6 @@ public class EnemyScript : MonoBehaviour
         float verticalWeaponDistance = 0.33f;
         const float weaponHeight = 0f;
         var handleRotation = _meHandle.GetRotation();
-
         float weaponRotation = (playerIsLeftOfEnemy) ? -25f : 25f;
         
         var up = new Vector3(0, -weaponRotation, 0);
